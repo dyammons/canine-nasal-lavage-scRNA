e0b5d012a69fc378ab98e318d26ade68069e9905
dyammons <dyammons95@gmail.com>
2024-02-01 08:45:56 -0700

    Run: integrate CanFam3.1 aligned data w/ log norm
    This script was used to integrate data that were aligned using
    CanFam3.1 genome build. This modification was made because I
    was unintentionally using the GSD reference, so need to make sure
    results are comperable, as I have been using CanFam3.1 for all
    previous projects.
    
    Files:
     - nasal_lavage_n8_log_canFam_S3.rds
     - nasal_lavage_n8_log_canFam_integrated_S2.rds
    
    JOBID: 4568471

--- 1_preprocessData.R
+++ 1_preprocessData.R
@@ -6,22 +6,22 @@ source("/pl/active/dow_lab/dylan/repos/scrna-seq/analysis-code/customFunctions_S
 ######################################## <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 #######   begin preprocessing   ######## <<<<<<<<<<<<<<
 ######################################## <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-outName <- "nasal_lavage_n8_sct"
+outName <- "nasal_lavage_n8_log_canFam"
 
-# load10x(din = "../input/", dout = "../output/s1/", outName = outName, testQC = FALSE,
-#                     nFeature_RNA_high = 9500, nFeature_RNA_low = 200, 
-#                     nCount_RNA_high = 150000, nCount_RNA_low = 100, 
-#                     percent.mt_high = 12.5, mt_pattern = "^MT-",
-#                     nfeatures = 2000,
-#                     removeDubs = TRUE, removeRBC_pal = FALSE, 
-#                     pal_feats = NULL, isolatePalRBC = FALSE,
-#                     featPlots = c("PTPRC", "CD3E", "CD8A", "GZMA", 
-#                                   "IL7R", "ANPEP", "FLT3", "DLA-DRA", 
-#                                   "CD4", "MS4A1", "PPBP","HBM")
-#                    )
+load10x(din = "../input_CanFam3.1/", dout = "../output/s1_cfam/", outName = outName, testQC = F,
+                    nFeature_RNA_high = 9000, nFeature_RNA_low = 200, 
+                    nCount_RNA_high = 125000, nCount_RNA_low = 100, 
+                    percent.mt_high = 12.5, mt_pattern = "^MT-",
+                    nfeatures = 2000,
+                    removeDubs = TRUE, removeRBC_pal = FALSE, 
+                    pal_feats = NULL, isolatePalRBC = FALSE,
+                    featPlots = c("PTPRC", "CD3E", "CD8A", "GZMA", 
+                                  "IL7R", "ANPEP", "FLT3", "DLA-DRA", 
+                                  "CD4", "MS4A1", "PPBP","HBM")
+                   )
 
 #integrate the data using all of the four Seurat v5 integration methods
-seu.obj <- integrateData(din = "../output/s1/", dout = "../output/s2/", outName = outName, normalization.method = "SCT",
+seu.obj <- integrateData(din = "../output/s1_cfam/", dout = "../output/s2/", outName = outName, normalization.method = "LogNormalize",
                          runAllMethods = TRUE, indReClus = F)
 
 #use clustree to identify clustering parameters that appear most appropriate
@@ -30,16 +30,15 @@ seu.obj <- integrateData(din = "../output/s1/", dout = "../output/s2/", outName 
 
 #complete data visualization
 for (x in list("integrated.cca", "integrated.harmony", "integrated.joint", "integrated.rcpa")) {
-    seu.obj <- dataVisUMAP(seu.obj = seu.obj, outDir = "../output/s3/", outName = x, 
-                           final.dims = 30, final.res = 0.6, stashID = "clusterID", algorithm = 3, min.dist = 0.1, n.neighbors = 10,
-                           prefix = "SCT_snn_res.", assay = "SCT", reduction = x,
+    seu.obj <- dataVisUMAP(seu.obj = seu.obj, outDir = "../output/s3/", outName = paste0(outName, "_", x), 
+                           final.dims = 45, final.res = 0.6, stashID = "clusterID", algorithm = 3, min.dist = 0.1, n.neighbors = 10,
+                           prefix = "RNA_snn_res.", assay = "RNA", reduction = x,
                            saveRDS = F, return_obj = T, returnFeats = T,
                            features = c("PTPRC", "CD3E", "CD8A", "GZMA", 
                                         "IL7R", "ANPEP", "FLT3", "DLA-DRA", 
                                         "CD4", "MS4A1", "PPBP","HBM")
                           )
 }
-
 
 saveRDS(seu.obj, paste0("../output/s3/", outName,"_S3.rds"))
 
