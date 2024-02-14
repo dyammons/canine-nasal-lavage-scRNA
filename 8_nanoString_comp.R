e0db44a08231b26325d297d86ae158d8cd12e5cc
dyammons <dyammons95@gmail.com>
2024-02-14 07:26:10 -0700

    Add 8_nanoString_comp.R for side-by-side analysis of paired data

---
+++ 8_nanoString_comp.R
@@ -0,0 +1,9 @@
+#!/usr/bin/Rscript
+
+#load custom functions & packages
+source("/pl/active/dow_lab/dylan/repos/scrna-seq/analysis-code/customFunctions_Seuratv5.R")
+
+######################################## <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+#######   begin preprocessing   ######## <<<<<<<<<<<<<<
+######################################## <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+outName <- "nasal_lavage_nanoString_comp"
