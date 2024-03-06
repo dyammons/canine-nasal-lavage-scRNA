9747de8259e416f1d9d2ef98193f6cc6aa286cae
dyammons <dyammons95@gmail.com>
2024-03-05 19:44:56 -0700

    Update clean neutrophil and run DGEA
    The previous 240202_lav_neut_n8_2000_log_cfam_clean_S3.rds did not
    exist for some reason, so re-created it from line 41 of this commit.
    Generated figures for the clean object, then moved onto DGE analysis.
    This revealed notable changes, but with opposing (or otherwise independent)
    changes when comparing d14 to d0 and d90 to d0. This is conistent with
    earlier analysis and provides strong evidence that d90 and d14 samples
    should NOT be collapsed into one post-treatment group.
    
    TO DO: look closer to ensure something is not being flipped, as this is an
    unexpected finding.

--- 3_neut_subset.R
+++ 3_neut_subset.R
@@ -41,10 +41,10 @@ saveRDS(seu.obj, paste0("../output/s3/", outName,"_S3.rds"))
 seu.obj <- readRDS(paste0("../output/s3/", outName,"_S3.rds"))
 
 
-#inspect inciting clusters
+#inspect clusters
 pi <- DimPlot(seu.obj, 
                 reduction = reduction, 
-                group.by = "clusterID",
+                group.by = "clusterID_integrated.harmony",
 #                 cols = colArray$newCol,
                 pt.size = 0.25,
                 label = TRUE,
@@ -54,6 +54,7 @@ p <- cusLabels(plot = pi, shape = 21, size = 8, alpha = 0.8, labCol = "black", s
 ggsave(paste0("../output/", outName, "/", outName, "_clusID_orig_UMAP.png"), width = 7, height = 7)
 
 
+#remove clusters 8& 9 
 Idents(seu.obj) <- "clusterID_integrated.harmony"
 seu.obj.sub <- subset(seu.obj, invert = T, idents = c(8,9))
 table(seu.obj.sub$clusterID_integrated.harmony)
@@ -154,8 +155,8 @@ pi <- DimPlot(seu.obj.ds,
 p <- formatUMAP(pi) + labs(colour="Cell source:") + theme(legend.position = "top", 
                                                             legend.direction = "horizontal",
                                                             legend.title=element_text(size=12)
-                                                            ) + guides(colour = guide_legend(nrow = 1, override.aes = list(size = 4)))
-ggsave(paste0("../output/", outName, "/", outName, "_umap_bySample.png"), width =7, height = 7)
+                                                            ) + guides(colour = guide_legend(nrow = 2, override.aes = list(size = 4)))
+ggsave(paste0("../output/", outName, "/", outName, "_umap_bySample.png"), width = 7, height = 7.5)
 
 
 ### Stacked bar graph by clusMain
@@ -169,12 +170,12 @@ ggsave(paste0("../output/", outName, "/", outName, "_stackedBar_cluster.png"), w
 
 
 ### Frequency plots to run stats - cluster
-freqy <- freqPlots(seu.obj, method = 1, nrow = 3, 
+freqy <- freqPlots(seu.obj, method = 1, nrow = 2, 
                    comp = "cellSource", groupBy = clusMain, legTitle = "Cell source", refVal = "name",
                    namez = "name", 
                    colz = "colz"
-)
-ggsave(paste0("../output/", outName, "/",outName, "_freqPlots_cluster.png"), width = 12, height = 8)
+) + NoLegend()
+ggsave(paste0("../output/", outName, "/",outName, "_freqPlots_cluster.png"), width = 6, height = 6)
 
 
 #run paired analysis -- TO DO: switch to RM ANOVA
@@ -184,6 +185,187 @@ lapply(unique(df$clusterID_integrated.harmony), function(x){
     df.sub <- filter(df, clusterID_integrated.harmony == x)
     wilcox.test(freq ~ cellSource, data = df.sub)
 })
+
+
+### Complete pseudobulk DGE by all cells
+createPB(seu.obj = seu.obj, groupBy = "allCells", comp = "cellSource", biologicalRep = "name", lowFilter = T, dwnSam = F, 
+         clusters = NULL, outDir = paste0("../output/", outName, "/pseudoBulk/")
+)
+
+#add dog metadata to account for paired nature of the data
+df <- read.csv(paste0("../output/", outName, "/pseudoBulk/allCells_deg_metaData.csv"), row.names = 1)
+df$dog <- unlist(lapply(df$sampleID, function(x){strsplit(x, "_")[[1]][2]}))
+df$tp <- c(1,1,1,1,3,3,2,2)
+write.csv(df, paste0("../output/", outName, "/pseudoBulk/allCells_deg_metaData.csv"))
+
+pseudoDEG(metaPWD = paste0("../output/", outName, "/pseudoBulk/allCells_deg_metaData.csv"),
+          padj_cutoff = 0.05, lfcCut = 0.58, outDir = paste0("../output/", outName, "/pseudoBulk/"), 
+          outName = outName, 
+          paired = T, pairBy = "dog", test.use = "Wald", strict_lfc = F,
+          idents.1_NAME = contrast[1], idents.2_NAME = contrast[2],
+          inDir = paste0("../output/", outName, "/pseudoBulk/"), title = "All cells", 
+          filterTerm = "ZZZZ", addLabs = NULL, mkDir = T
+)
+
+
+
+### Complete GSEA using the linDEG results
+df <- read.csv(paste0("../output/", outName, "/pseudoBulk/allCells/", outName, "_cluster_allCells_all_genes.csv")) %>% arrange(padj)
+
+upGenes <- df %>% filter(log2FoldChange > 0) %>% pull(gene)
+dwnGenes <- df %>% filter(log2FoldChange < 0) %>% pull(gene)
+p <- plotGSEA(geneList = upGenes, geneListDwn = dwnGenes, category = "C5", subcategory = "GO:BP", 
+              upCol = "red", dwnCol = "blue", size = 4)
+
+minVal <- -15
+maxVal <- 5
+pi <- p + scale_x_continuous(limits = c(minVal, maxVal), name = "Signed log10(padj)")
+ggsave(paste("../output/", outName, "/", outName, "_allCells_gsea.png", sep = ""), width = 7, height = 4)
+
+
+top.degs <- read.csv(paste0("../output/", outName, "/pseudoBulk/allCells/", outName, "_cluster_allCells_all_genes.csv")) %>% filter(log2FoldChange > 0) %>% arrange(padj) %>% pull(gene)
+
+### Plot top DEGS
+features <- top.degs[c(1,2,4:6,8,9,11)]
+set.seed(12)
+Idents(seu.obj) <- "cellSource"
+seu.obj.sub <- subset(seu.obj, downsample = min(table(seu.obj$cellSource)))
+p <- FeaturePlot(seu.obj.sub,features = features, 
+                 reduction = reduction, pt.size = 0.01, 
+                 split.by = "cellSource", order = T, 
+                 by.col = F) + labs(x = "UMAP1", y = "UMAP2") & theme(axis.text = element_blank(),
+                                                                      axis.title.y.right = element_text(size = 16),
+                                                                      axis.ticks = element_blank(),
+                                                                      axis.title = element_blank(),
+                                                                      axis.line = element_blank(),
+                                                                      plot.title = element_text(size=16),
+                                                                      title = element_blank(),
+                                                                      plot.margin = unit(c(1, 0, 0, 0), "pt")
+                                                                      ) & scale_color_gradient(breaks = pretty_breaks(n = 3), limits = c(NA, NA), low = "lightgrey", high = "darkblue")
+ggsave(paste0("../output/", outName, "/", outName, "_split_degs.png"), width = 16, height = 4)
+
+
+
+########## BONUS ##########
+
+### Investigate if d14 or d90 is driving DEGs
+Idents(seu.obj) <- "name"
+seu.obj.sub <- subset(seu.obj, idents = c("pre_1", "pre_2", "post_1", "post_2"))
+table(seu.obj.sub$name)
+
+seu.obj.sub$d90 <- "d90"
+seu.obj.sub$d90 <- as.factor(seu.obj$d90)
+### Complete pseudobulk DGE by all cells
+createPB(seu.obj = seu.obj.sub, groupBy = "d90", comp = "cellSource", biologicalRep = "name", lowFilter = T, dwnSam = F, 
+         clusters = NULL, outDir = paste0("../output/", outName, "/pseudoBulk/")
+)
+
+#add dog metadata to account for paired nature of the data
+df <- read.csv(paste0("../output/", outName, "/pseudoBulk/d90_deg_metaData.csv"), row.names = 1)
+df$dog <- unlist(lapply(df$sampleID, function(x){strsplit(x, "_")[[1]][2]}))
+write.csv(df, paste0("../output/", outName, "/pseudoBulk/d90_deg_metaData.csv"))
+
+pseudoDEG(metaPWD = paste0("../output/", outName, "/pseudoBulk/d90_deg_metaData.csv"),
+          padj_cutoff = 0.05, lfcCut = 0.58, outDir = paste0("../output/", outName, "/pseudoBulk/"), 
+          outName = outName, 
+          paired = T, pairBy = "dog", test.use = "Wald", strict_lfc = F,
+          idents.1_NAME = contrast[1], idents.2_NAME = contrast[2],
+          inDir = paste0("../output/", outName, "/pseudoBulk/"), title = "d90 vs d0", 
+          filterTerm = "ZZZZ", addLabs = NULL, mkDir = T
+)
+
+top.degs <- read.csv(paste0("../output/", outName, "/pseudoBulk/d90/", outName, "_cluster_d90_all_genes.csv")) %>% filter(log2FoldChange < 0) %>% arrange(padj) %>% pull(gene)
+
+### Plot top DEGS
+features <- top.degs[1:8]
+set.seed(12)
+Idents(seu.obj) <- "cellSource"
+seu.obj.sub <- subset(seu.obj, downsample = min(table(seu.obj$cellSource)))
+p <- FeaturePlot(seu.obj.sub,features = features, 
+                 reduction = reduction, pt.size = 0.01, 
+                 split.by = "cellSource", order = T, 
+                 by.col = F) + labs(x = "UMAP1", y = "UMAP2") & theme(axis.text = element_blank(),
+                                                                      axis.title.y.right = element_text(size = 16),
+                                                                      axis.ticks = element_blank(),
+                                                                      axis.title = element_blank(),
+                                                                      axis.line = element_blank(),
+                                                                      plot.title = element_text(size=16),
+                                                                      title = element_blank(),
+                                                                      plot.margin = unit(c(1, 0, 0, 0), "pt")
+                                                                      ) & scale_color_gradient(breaks = pretty_breaks(n = 3), limits = c(NA, NA), low = "lightgrey", high = "darkblue")
+ggsave(paste0("../output/", outName, "/", outName, "_split_d90_degs.png"), width = 16, height = 4)
+
+
+
+Idents(seu.obj) <- "name"
+seu.obj.sub <- subset(seu.obj, idents = c("pre_3", "pre_4", "post_3", "post_4"))
+table(seu.obj.sub$name)
+
+seu.obj.sub$d14 <- "d14"
+seu.obj.sub$d14 <- as.factor(seu.obj$d14)
+### Complete pseudobulk DGE by all cells
+createPB(seu.obj = seu.obj.sub, groupBy = "d14", comp = "cellSource", biologicalRep = "name", lowFilter = T, dwnSam = F, 
+         clusters = NULL, outDir = paste0("../output/", outName, "/pseudoBulk/")
+)
+
+#add dog metadata to account for paired nature of the data
+df <- read.csv(paste0("../output/", outName, "/pseudoBulk/d14_deg_metaData.csv"), row.names = 1)
+df$dog <- unlist(lapply(df$sampleID, function(x){strsplit(x, "_")[[1]][2]}))
+write.csv(df, paste0("../output/", outName, "/pseudoBulk/d14_deg_metaData.csv"))
+
+pseudoDEG(metaPWD = paste0("../output/", outName, "/pseudoBulk/d14_deg_metaData.csv"),
+          padj_cutoff = 0.05, lfcCut = 0.58, outDir = paste0("../output/", outName, "/pseudoBulk/"), 
+          outName = outName, 
+          paired = T, pairBy = "dog", test.use = "Wald", strict_lfc = F,
+          idents.1_NAME = contrast[1], idents.2_NAME = contrast[2],
+          inDir = paste0("../output/", outName, "/pseudoBulk/"), title = "d14 vs d0", 
+          filterTerm = "ZZZZ", addLabs = NULL, mkDir = T
+)
+
+
+top.degs <- read.csv(paste0("../output/", outName, "/pseudoBulk/d14/", outName, "_cluster_d14_all_genes.csv")) %>% filter(log2FoldChange > 0) %>% arrange(padj) %>% pull(gene)
+
+### Plot top DEGS
+features <- top.degs[c(2:6,8,9)]
+set.seed(12)
+Idents(seu.obj) <- "cellSource"
+seu.obj.sub <- subset(seu.obj, downsample = min(table(seu.obj$cellSource)))
+p <- FeaturePlot(seu.obj.sub,features = features, 
+                 reduction = reduction, pt.size = 0.01, 
+                 split.by = "cellSource", order = T, 
+                 by.col = F) + labs(x = "UMAP1", y = "UMAP2") & theme(axis.text = element_blank(),
+                                                                      axis.title.y.right = element_text(size = 16),
+                                                                      axis.ticks = element_blank(),
+                                                                      axis.title = element_blank(),
+                                                                      axis.line = element_blank(),
+                                                                      plot.title = element_text(size=16),
+                                                                      title = element_blank(),
+                                                                      plot.margin = unit(c(1, 0, 0, 0), "pt")
+                                                                      ) & scale_color_gradient(breaks = pretty_breaks(n = 3), limits = c(NA, NA), low = "lightgrey", high = "darkblue")
+ggsave(paste0("../output/", outName, "/", outName, "_split_d14_degs.png"), width = 16, height = 4)
+
+
+### DGE intersection analysis
+top.degs.d90 <- read.csv(paste0("../output/", outName, "/pseudoBulk/d90/", outName, "_cluster_d90_all_genes.csv")) %>% filter(log2FoldChange > 0) %>% arrange(padj) %>% pull(gene)
+top.degs.d14 <- read.csv(paste0("../output/", outName, "/pseudoBulk/d14/", outName, "_cluster_d14_all_genes.csv")) %>% filter(log2FoldChange > 0) %>% arrange(padj) %>% pull(gene)
+intersect(top.degs.d90, top.degs.d14)
+# [1] "GBP1"
+
+top.degs.d90 <- read.csv(paste0("../output/", outName, "/pseudoBulk/d90/", outName, "_cluster_d90_all_genes.csv")) %>% filter(log2FoldChange < 0) %>% arrange(padj) %>% pull(gene)
+top.degs.d14 <- read.csv(paste0("../output/", outName, "/pseudoBulk/d14/", outName, "_cluster_d14_all_genes.csv")) %>% filter(log2FoldChange < 0) %>% arrange(padj) %>% pull(gene)
+intersect(top.degs.d90, top.degs.d14)
+# character(0)
+
+top.degs.all <- read.csv(paste0("../output/", outName, "/pseudoBulk/allCells/", outName, "_cluster_allCells_all_genes.csv")) %>% filter(log2FoldChange > 0) %>% arrange(padj) %>% pull(gene)
+top.degs.d90 <- read.csv(paste0("../output/", outName, "/pseudoBulk/d90/", outName, "_cluster_d90_all_genes.csv")) %>% filter(log2FoldChange > 0) %>% arrange(padj) %>% pull(gene)
+intersect(top.degs.d90, top.degs.all)
+# [1] "CCR1" "GBP1"
+
+
+top.degs.all <- read.csv(paste0("../output/", outName, "/pseudoBulk/allCells/", outName, "_cluster_allCells_all_genes.csv")) %>% filter(log2FoldChange > 0) %>% arrange(padj) %>% pull(gene)
+top.degs.d14 <- read.csv(paste0("../output/", outName, "/pseudoBulk/d14/", outName, "_cluster_d14_all_genes.csv")) %>% filter(log2FoldChange > 0) %>% arrange(padj) %>% pull(gene)
+intersect(top.degs.d90, top.degs.d14)
+# [1] "GBP1"
 
 ################################# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 #######   end analysis   ######## <<<<<<<<<<<<<<
